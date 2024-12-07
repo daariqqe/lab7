e4b566b2877fdc40568221d6e50c618f33c92681
Daria Moldovan <dassshhhhkkaa@gmail.com>
2024-12-07 23:34:02 +0200

    Add files via upload

---
+++ ЛР7/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ЛР7
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.TextBox tbMinResult; 
+        private System.Windows.Forms.Label labelX1;
+        private System.Windows.Forms.Label labelX2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelMinResult; 
+
+        private void InitializeComponent()
+        {
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.labelX2 = new System.Windows.Forms.Label();
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.tbMinResult = new System.Windows.Forms.TextBox(); 
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelMinResult = new System.Windows.Forms.Label(); 
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+
+            // labelX1
+            this.labelX1.Text = "X1:";
+            this.labelX1.Location = new System.Drawing.Point(30, 20);
+            this.labelX1.Size = new System.Drawing.Size(50, 20);
+
+            // labelX2
+            this.labelX2.Text = "X2:";
+            this.labelX2.Location = new System.Drawing.Point(30, 60);
+            this.labelX2.Size = new System.Drawing.Size(50, 20);
+
+            // tbX1
+            this.tbX1.Location = new System.Drawing.Point(90, 20);
+            this.tbX1.Size = new System.Drawing.Size(150, 20); 
+            this.tbX1.ForeColor = System.Drawing.Color.Black; 
+            this.tbX1.BackColor = System.Drawing.Color.White; 
+            // tbX2
+            this.tbX2.Location = new System.Drawing.Point(90, 60);
+            this.tbX2.Size = new System.Drawing.Size(150, 20);
+            this.tbX2.ForeColor = System.Drawing.Color.Black;
+            this.tbX2.BackColor = System.Drawing.Color.White;
+
+
+            // btnCalculate
+            this.btnCalculate.Text = "Обчислити";
+            this.btnCalculate.Location = new System.Drawing.Point(30, 100);
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+
+            // tbResult
+            this.tbResult.Location = new System.Drawing.Point(90, 140);
+            this.tbResult.Size = new System.Drawing.Size(150, 20);
+            this.tbResult.ReadOnly = true;
+            this.tbResult.ForeColor = System.Drawing.Color.Black;
+            this.tbResult.BackColor = System.Drawing.Color.LightGray;
+
+            // tbMinResult
+            this.tbMinResult.Location = new System.Drawing.Point(105, 180);
+            this.tbMinResult.Size = new System.Drawing.Size(150, 20);
+            this.tbMinResult.ReadOnly = true;
+            this.tbMinResult.ForeColor = System.Drawing.Color.Black;
+            this.tbMinResult.BackColor = System.Drawing.Color.LightGray;
+
+            // labelResult
+            this.labelResult.Text = "Результат:";
+            this.labelResult.Location = new System.Drawing.Point(30, 140);
+            this.labelResult.Size = new System.Drawing.Size(60, 20);
+
+            // labelMinResult
+            this.labelMinResult.Text = "Мін. значення";
+            this.labelMinResult.Location = new System.Drawing.Point(30, 180);
+            this.labelMinResult.Size = new System.Drawing.Size(100, 20);
+
+            // btnClear
+            this.btnClear.Text = "Очистити";
+            this.btnClear.Location = new System.Drawing.Point(30, 220);
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+
+            // btnExit
+            this.btnExit.Text = "Вихід";
+            this.btnExit.Location = new System.Drawing.Point(130, 220);
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.tbX1);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbMinResult);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelMinResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Text = "Лабораторна робота 7";
+        }
+    }
+}
---
+++ ЛР7/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ЛР7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                double x1 = double.Parse(tbX1.Text);
+                double x2 = double.Parse(tbX2.Text);
+
+                if (x1 < 0)
+                {
+                    MessageBox.Show("x1 має бути >= 0 для коректного обчислення.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                double y = Math.Sin(x1 - Math.Pow(x2, 3) + Math.Sqrt(x1)) - 1.3 * Math.Pow(x1, 3);
+
+                tbResult.Text = y.ToString("F4");
+
+                double minValue = Math.Min(x1, x2);
+                tbMinResult.Text = minValue.ToString("F4"); 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Перевірте, чи правильно введені дані (числа).", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сталася помилка: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbX1.Clear();
+            tbX2.Clear();
+            tbResult.Clear();
+            tbMinResult.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            btnCalculate_Click(sender, e);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            btnClear_Click(sender, e); 
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            btnExit_Click(sender, e); 
+        }
+
+    }
+}
